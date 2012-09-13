4a62d7440179e53dd9687155bae8b5a5aaeb0443
Niek <niekversteege@gmail.com>
2012-09-13 21:06:17 +0200

    Init

---
+++ BinaryTrees/src/nl/niek/minor/aa/binarytrees/BinaryTreesMain.java
@@ -0,0 +1,13 @@
+package nl.niek.minor.aa.binarytrees;
+
+public class BinaryTreesMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
